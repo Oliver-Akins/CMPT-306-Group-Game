46a95e0b51bf65f9af8cf2f76e404bb030f52a38
keromyru <aurora.dubnyk88@gmail.com>
2021-10-31 21:19:23 -0600

    Added in some extra things to melee attack, it needs to check enemy hits, and manage damage, also eventually needs to be modulated by a specific weapon damage and the players stats

--- Assets/Scripts/Melee_Attack.cs
+++ Assets/Scripts/Melee_Attack.cs
@@ -7,25 +7,35 @@ public class Melee_Attack : MonoBehaviour {
 	public Animator animator;
 	public Transform meleeAttackPoint;
 	public float attackRange = 0.5f;
-	
+	// limit player attack time, increase with agility!
+	private float timeBetweenAttacks;
+	public float startTimeBetweenAttacks;
+	// damaging enemies may need to change
 	public LayerMask enemyLayers;
 	// Update is called once per frame
 	void Update() {
-		if (Input.GetButtonDown("Fire1")){
-			Attack();
-		}
+		if (timeBetweenAttacks <= 0){
+			if (Input.GetButtonDown("Fire1")){
+				Attack();
+				timeBetweenAttacks = startTimeBetweenAttacks;
+			}
+		} else {
+				timeBetweenAttacks -= Time.deltaTime;
+			}
+		
 	}
 
 	void Attack(){
-		// attack animation trigger (in animator is the trigger param )
+		// attack animation trigger (in animator this is the trigger param)
 		animator.SetTrigger("Attack");
-		// detect enemies in range
+		// detect enemies in range/hit, they need to be on the enemy layer btw
 		Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(meleeAttackPoint.position, attackRange, enemyLayers);
 
-		// damage the enemies the player hits, obviously we need enemies to have 
-		// health and colliders first
+		/** damage the enemies the player hits, obviously we need enemies to have
+		health and colliders first obviously
+		*/
 		foreach(Collider2D enemy in hitEnemies){
-			Debug.Log("We hit" + enemy.name);
+			Debug.Log("We hit" + enemy.name); 
 		}
 	}
 
