a1dccbc9a27692a4cc922e85e9808726d9519b1a
Kelly Neuda <kvn904@mail.usask.ca>
2021-11-26 16:06:37 -0600

    added a randomized number of coin drops during killstreak

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -76,8 +76,21 @@ public class EnemyController : MonoBehaviour {
 		// can have a death effect to if we want
 		Destroy(gameObject);
 
-		// Instantiate(lootDrop, transform.position, Quaternion.identity);
-		Instantiate(items[Random.Range(0, items.Count-1)], transform.position, Quaternion.identity);
+		if(AchievementCollection.killStreak > 4) {
+			int coins = Random.Range(2, 5);
+
+			for(int i = 0; i < coins; i++) {
+				Instantiate(items[0], transform.position + new Vector3(Random.Range(-.5f, .5f), Random.Range(-.5f, .5f), 0), Quaternion.identity);
+			}
+
+			Instantiate(items[Random.Range(1, items.Count-1)], transform.position, Quaternion.identity);
+
+		} else {
+			Instantiate(items[Random.Range(0, items.Count-1)], transform.position, Quaternion.identity);
+		}
+
+		
+	
 	}
 
 	public void Attack(){
